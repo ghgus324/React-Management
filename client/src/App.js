52a0a7a235710c8ae1a40a79cdf7897a91ef0196
kimhohyeon <ghgus323@naver.com>
2019-06-10 01:25:04 +0900

    Add Get Customers REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -13,40 +13,35 @@ import { withStyles } from '@material-ui/core/styles';
 const styles = theme => ({
   root: {
     width: '100%',
-    marginTop: theme.spacing.unit *3,
-    overflowX:"auto"
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
   },
-  table:{
+  table: {
     minWidth: 1080
   }
 })
 
 
-
-const customers = [{
-  'id': 'hong',
-  'name': '홍길동',
-  'image': 'https://placeimg.com/64/64/1',
-  'birthday': 961222,
-  'gender': '남자',
-  'job': '대학생'
-}, {
-  'id': 'lee',
-  'name': '이순신',
-  'image': 'https://placeimg.com/64/64/2',
-  'birthday': 921222,
-  'gender': '남자',
-  'job': '프로그래머'
-}, {
-  'id': 'an',
-  'name': '앙드레 김',
-  'image': 'https://placeimg.com/64/64/3',
-  'birthday': 961222,
-  'gender': '남자',
-  'job': '디자이너'
-}]
-
 class App extends Component {
+
+  state = {
+    customers: ""
+  }
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
+  }
+
+  //비동기적으로 작업
+  callApi = async () => {
+     const response = await fetch('/api/customers');
+     const body = await response.json();
+     return body;
+  }
+
+
   render() {
     const { classes } = this.props;
     return (
@@ -64,7 +59,7 @@ class App extends Component {
           </TableHead>
 
           <TableBody>
-            {customers.map(c => {
+            {this.state.customers ? this.state.customers.map(c => {
               return (
                 <Customer
                   key={c.id}
@@ -75,7 +70,7 @@ class App extends Component {
                   gender={c.gender}
                   job={c.job} />
               )
-            })}
+            }) : ""}
           </TableBody>
         </Table>
       </Paper>
--- server.js
+++ server.js
@@ -8,8 +8,31 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
 //api/hello 경로로 들어오면 어떤 작업을 해줄 건지
-app.get('/api/hello',(req,res) =>{
-    res.send({message: 'Hello Express!'})
+app.get('/api/customers',(req,res)=>{
+    res.send([
+        {
+            'id': 'hong',
+            'name': '홍길동',
+            'image': 'https://placeimg.com/64/64/1',
+            'birthday': 961222,
+            'gender': '남자',
+            'job': '대학생'
+          }, {
+            'id': 'lee',
+            'name': '이순신',
+            'image': 'https://placeimg.com/64/64/2',
+            'birthday': 921222,
+            'gender': '남자',
+            'job': '프로그래머'
+          }, {
+            'id': 'an',
+            'name': '앙드레 김',
+            'image': 'https://placeimg.com/64/64/3',
+            'birthday': 961222,
+            'gender': '남자',
+            'job': '디자이너'
+          }
+    ]);
 });
 
 app.listen(port , () => console.log(`Listening on port ${port}`));
