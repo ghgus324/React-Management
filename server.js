631f7afbe785a2c5e498064e6ff7292ba69552ff
kimhohyeon <ghgus323@naver.com>
2019-06-10 22:45:24 +0900

    Create Customer Database Table & Node.js Database Connection setting

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 // React Web Server(express server)
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -7,32 +8,27 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
+const data =fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+host: conf.host,
+user: conf.user,
+password: conf.password,
+port: conf.port,
+database: conf.database
+});
+connection.connect();
+
 //api/hello 경로로 들어오면 어떤 작업을 해줄 건지
 app.get('/api/customers',(req,res)=>{
-    res.send([
-        {
-            'id': 'hong',
-            'name': '홍길동',
-            'image': 'https://placeimg.com/64/64/1',
-            'birthday': 961222,
-            'gender': '남자',
-            'job': '대학생'
-          }, {
-            'id': 'lee',
-            'name': '이순신',
-            'image': 'https://placeimg.com/64/64/2',
-            'birthday': 921222,
-            'gender': '남자',
-            'job': '프로그래머'
-          }, {
-            'id': 'an',
-            'name': '앙드레 김',
-            'image': 'https://placeimg.com/64/64/3',
-            'birthday': 961222,
-            'gender': '남자',
-            'job': '디자이너'
-          }
-    ]);
+    connection.query(
+      "SELECT * FROM CUSTOMER",
+      (err,rows,fields) => {
+        res.send(rows);
+      }
+    );
 });
 
 app.listen(port , () => console.log(`Listening on port ${port}`));
